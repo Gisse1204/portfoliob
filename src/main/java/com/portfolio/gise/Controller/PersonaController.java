22dd7ee5c6c60e5eeb6e916b712328666f70d4b4
Gisse1204 <gise_17@hotmail.com>
2023-04-25 19:49:49 -0300

    error en una "s"

--- src/main/java/com/portfolio/gise/Controller/PersonaController.java
+++ src/main/java/com/portfolio/gise/Controller/PersonaController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaservice;
-    @GetMapping("personas/traer")
+    @GetMapping("/personas/traer")
     public List<Persona> getPersona(){
         return ipersonaservice.getPersona();
     }
