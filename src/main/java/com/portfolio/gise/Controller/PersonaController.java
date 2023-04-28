60ddbb7e2832b992a5275d3bfb44fe53dbedde12
Gisse1204 <gise_17@hotmail.com>
2023-04-27 21:46:33 -0300

    arreglos

--- src/main/java/com/portfolio/gise/Controller/PersonaController.java
+++ src/main/java/com/portfolio/gise/Controller/PersonaController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:4200")
 @CrossOrigin(origins = "https://portfoliogisse.web.app")
 public class PersonaController {
     @Autowired IPersonaService ipersonaservice;
--- src/main/java/com/portfolio/gise/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/gise/Security/Controller/AuthController.java
@@ -28,7 +28,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@CrossOrigin(origins = "http://localhost:4200")
 @CrossOrigin(origins = "https://portfoliogisse.web.app")
 @RestController
 @RequestMapping("/auth")
